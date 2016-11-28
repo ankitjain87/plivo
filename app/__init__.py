a343890bee19ab84bc130f4c506aeb2ca674a047
Ankit Jain <anjain@akamai.com>
2016-11-28 12:09:54 +0530

    Updated init.py to support heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 #!flask/bin/python
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.heroku import Heroku
+
 import redis
 
 import config
@@ -8,6 +10,7 @@ import config
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_URI
 
+heroku = Heroku(app)
 cache = redis.Redis(config.REDIS_URL)
 db = SQLAlchemy(app)
 
