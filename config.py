4a440520699a73ade49bff61bc97f76bc0098542
Ankit Jain <anjain@akamai.com>
2016-12-18 11:17:35 +0530

    Updated postres uri.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 REDIS_URL = 'localhost'
-DB_URI = 'postgresql://localhost/anjain'
+DB_URI = 'postgres://fvygwugckjtwkm:f231d7c9009e9ba167be9eeebe27ab9c43e9201d8468de1bbc4734b80955349f@ec2-23-23-225-116.compute-1.amazonaws.com:5432/d8uck3k3kk5jqs'
 
 API_LIMIT = 50
 CACHE_EXPIRE = 14400    # 4 * 60 * 60
